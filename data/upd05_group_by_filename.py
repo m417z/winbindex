b89a9082e960817d304f684e8becbdac8c7c9321
m417z <mmm.maltsev@gmail.com>
2020-08-02 01:45:33 +0300

    Travis performance test (3)

--- data/upd05_group_by_filename.py
+++ data/upd05_group_by_filename.py
@@ -154,7 +154,7 @@ def add_file_info_from_update(filename, output_dir, *, file_hash, file_info, win
         file_info_data[filename] = data
     else:
         output_path = output_dir.joinpath(filename + '.json.gz')
-        with gzip.open(output_path, 'wt', encoding='utf-8') as f:
+        with gzip.open(output_path, 'wt', compresslevel=6, encoding='utf-8') as f:
             ujson.dump(data, f, indent=4, sort_keys=True)
 
 virustotal_info_cache = {}
@@ -315,7 +315,7 @@ def group_update_by_filename(windows_version, update_kb, update, parsed_dir):
             if count % 200 == 0:
                 print(f' ...{count}', end='', flush=True)
             if count == 1000:
-            	return
+            	exit('Aborted')
 
         try:
             group_update_assembly_by_filename(str(path), output_dir,
