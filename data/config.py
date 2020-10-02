fd76b2f210d2a339da841fea95a3b26402afe17d
m417z <mmm.maltsev@gmail.com>
2020-10-02 16:46:16 +0300

    Added asserts for duplicate detection (thanks KB3200970 and KB3198586)

--- data/config.py
+++ data/config.py
@@ -7,3 +7,13 @@ extract_in_a_new_thread = False
 exit_on_first_error = True
 high_mem_usage_for_performance = False
 compression_level = 9
+
+# Key: version to skip, value: version containing the same updates.
+windows_versions_to_skip = {
+    '1909': '1903',
+}
+
+windows_update_urls_to_skip = [
+    'https://support.microsoft.com/en-us/help/4001884',  # Same update as https://support.microsoft.com/en-us/help/4001883
+    'https://support.microsoft.com/en-us/help/4001886',  # Same update as https://support.microsoft.com/en-us/help/4001885
+]
