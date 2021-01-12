725aaf899dcedf18cbad65e4647a2721b7098943
m417z <mmm.maltsev@gmail.com>
2021-01-12 21:55:17 +0200

    Support for Windows versions with overlapping updates

--- data/upd05_group_by_filename.py
+++ data/upd05_group_by_filename.py
@@ -343,12 +343,14 @@ def process_updates(progress_state=None, time_to_stop=None):
         updates = {}
 
     for windows_version in updates:
-        if windows_version in config.windows_versions_to_skip:
-            continue
-
         print(f'Processing Windows version {windows_version}:', end='', flush=True)
 
+        older_windows_version = config.windows_with_overlapping_updates.get(windows_version)
+
         for update in updates[windows_version]:
+            if older_windows_version in updates and update in updates[older_windows_version]:
+                continue
+
             update_kb = update['updateKb']
             update_url = update['updateUrl']
             if update_url in config.windows_update_urls_to_skip:
