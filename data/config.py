65c4e0beff47391fe117216d6a92ead06073fa5f
Michael Maltsev <4129781+m417z@users.noreply.github.com>
2026-07-29 13:24:16 +0300

    Add filenames to delta_data_without_rift_table_names

--- data/config.py
+++ data/config.py
@@ -60,6 +60,11 @@ delta_machine_type_values_supported = {
 delta_data_without_rift_table_names = {
     '*.mui',
     'stdole32.tlb',
+    'microsoft.hostcompute.interop.dll',
+    'microsoft.hostcompute.powershell.cmdlets.dll',
+    'microsoft.hostcompute.powershell.views.dll',
+    'microsoft.hyperv.schema.marshal.dll',
+    'microsoft.hyperv.schema.dll',
 }
 delta_data_without_rift_table_manifests = {
     'amd64_microsoft-nxt-boottocloud-windows365-app_*',
